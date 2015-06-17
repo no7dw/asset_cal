5322dd5c01f5df86db0c10082af798ce0780a24c
Wade <wdeng@kalengo.com>
2015-06-17 18:05:32 +0800

    first commit

---
+++ util/date_util.js
@@ -0,0 +1,29 @@
+
+var moment = require('moment');
+
+module.exports = {
+  /**
+   *
+   * @param currentTime 当前的时间
+   * @param addday 是否加减日期
+   * @returns {number}返回的是当日零点的时间戳
+   */
+  getDate: function (currentTime, addDay, isSetZero) {
+    if (currentTime == undefined)
+      currentTime = new Date();
+    var today = new Date();
+    if (addDay != undefined) {
+      today.setFullYear(currentTime.getFullYear(), currentTime.getMonth(), currentTime.getDate() + addDay);
+    } else {
+      today.setFullYear(currentTime.getFullYear(), currentTime.getMonth(), currentTime.getDate());
+    }
+    if(isSetZero) today.setHours(0, 0, 0, 0);
+    return today.getTime();
+  },
+  formatTime: function (currentTime, formatStr) {
+    var time = currentTime || new Date();
+    var format = formatStr || '';
+    return moment(time).format(format);
+  },
+  
+};
