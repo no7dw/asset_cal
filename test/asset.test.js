0e3ba7c6701a592661c33895470ef4526a6d25a1
no7david <no7david@gmail.com>
2015-07-18 18:25:21 +0800

    change more log & fix more condiction

--- test/asset.test.js
+++ test/asset.test.js
@@ -3,40 +3,51 @@ var should = require('should');
 var date_util = require('../util/date_util');
 
 describe('asset calucaltor', function(){	
-	it('verify array without time', function(done){	
-		var asset = new Asset('');
+	// it('verify array without time', function(done){	
+	// 	var asset = new Asset('');
 
-		var arr = [{
-			rate : 0.08,
-			num : 1000
-		},{
-			rate : 0.08,
-			num : 500
-		}
-		];
-		var result = asset.calArray(arr)
-		// console.log("result",result);
-		result.should.be.equal(0.3287671232876712);
-		done();
-	});
+	// 	var arr = [{
+	// 		rate : 0.08,
+	// 		num : 1000
+	// 	},{
+	// 		rate : 0.08,
+	// 		num : 500
+	// 	}
+	// 	];
+	// 	var result = asset.calArray(arr)
+	// 	// console.log("result",result);
+	// 	result.should.be.equal(0.3287671232876712);
+	// 	done();
+	// });
 	it('verify array with time', function(done){	
 		var asset = new Asset('');
-		var isSetZeroTime = true;
-		var arr = [{
-			rate : 0.08,
-			num : 1000,
-			start_time:new Date('2015-06-09').getTime(),
-			end_time:new Date('2015-06-16').getTime()//out of date
-		},{
-			rate : 0.08,
-			num : 500,
-			start_time:date_util.getDate(null, -3, isSetZeroTime),
-			end_time:date_util.getDate(null, +10, isSetZeroTime)
-		}
-		];
-		var result = asset.calArray(arr)
-		// console.log("result",result);
-		result.should.be.equal(0.08*1000/365);
+		// var isSetZeroTime = true;
+		// var arr = [{
+		// 	rate : 0.08,
+		// 	num : 1000,
+		// 	start_time:new Date('2015-06-09').getTime(),
+		// 	end_time:new Date('2015-06-16').getTime()//out of date
+		// },{
+		// 	rate : 0.08,
+		// 	num : 500,
+		// 	start_time:date_util.getDate(null, -3, isSetZeroTime),
+		// 	end_time:date_util.getDate(null, +10, isSetZeroTime)
+		// }
+		// ];
+
+		asset.on('custom_event',  function(result){
+			console.log('event result', result);
+		})
+		// var listener = asset.on('custom_event', function(err){
+		// 	console.log('on listener' , err);
+		// });
+
+		var result = asset.calArray([1,2])		
+		
+		// var listener = asset.emit('custom_event', 'time not set in emit');
+		// console.log("listener:",listener);
+		// result.should.be.equal(0.08*1000/365);
 		done();
+		
 	});
 });
