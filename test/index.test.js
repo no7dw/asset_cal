0376e4c300bfbb75776ffc93809a4375af2e62aa
no7david <no7david@gmail.com>
2015-10-13 11:51:05 +0800

    add ex_money case, add warning log

--- test/index.test.js
+++ test/index.test.js
@@ -14,7 +14,37 @@ var calculateSingleUserAsset = function (portfolioInfo, asset) {
     return af.cal(principalAsset);
 }
 
-
+//活期
+var demandDepositSample = {
+  "amount" : 11135.1943252244818723,
+  "product_id" : "549922452238c54e98b750bc",
+  "asset_id" : "55fa7b56de3259bf06ba985e",
+  "num" : 11135,
+  "earning" : 236.1943252244836344
+},
+//定期
+  timeDepositSample = {
+  "amount" : 3038.5802384521985005,
+  "num" : 3038,
+  "product_id" : "55c31e936f227ed922c508aa",
+  "asset_id" : "5602151906b18b627e809de1",
+  "rebuy" : 0,
+  //"start_time" : ISODate("2015-08-01T00:00:00.000Z"),
+  //"end_time" : ISODate("2015-11-01T00:00:00.000Z"),
+  "expect_earning" : 67.4315066615419454
+},
+//体验金
+  bonusSample = {
+    "ticket_id" : "558270b209da1ca263a3cc10",
+    "bonus_id" : "558270801c760de631b45970",
+    "bonus_type" : "ex_money",
+    "description" : "即送%s元体验金",
+    "amount" : 500,
+    "asset_id" : "561b8f2caf0eef937be5fefe",
+    "start_time" : 1444579200000.0000000000000000,
+    "end_time" : 1445184000000.0000000000000000,
+    //"time" : ISODate("2015-10-12T10:45:00.774Z")
+  };
 var funds = [ { quota: 10000,
     name: '三月定期',
     term: 90,
@@ -63,7 +93,34 @@ var portfolioMul = [
       "start_time": date_util.getDate(new Date(), -90),
       "portfolio_id": Const.PRODUCT.TIME3MID //55c31e936f227ed922c508aa timeDeposit.id
     }
-  ]
+  ];
+var assembleObjId = function (asset, type) {
+
+  asset.assetType = "demandDeposit";
+  if (type == 'asset') {
+    asset.objId = asset.asset_id || asset.portfolio_id;
+    asset.type = 2;
+    if(asset.product_id && asset.product_id != Const.PRODUCT.KOALAID )
+      asset.assetType = "timeDeposit";
+  }
+  else if (type == 'bonus') {
+    asset.product_id = Const.PRODUCT.KOALAID;
+    asset.objId = asset.invitee_id ? asset.ticket_id + '_' + asset.invitee_id : asset.ticket_id;
+    if (asset.bonus_type == 'ex_money') {
+      asset.type = 4;
+    } else if (asset.bonus_type == 'cash') {
+      asset.type = 3;
+    } else {
+      asset.type = 4;
+    }
+  }
+  asset.portfolio_id = asset.product_id || asset.portfolio_id;
+  return asset;
+};
+
+var testAssemble = function(bonus){
+  return assembleObjId(bonus, 'bonus');
+};
 
 describe('asset calucaltor', function(){
 	it('verify 错误的portfolio asset pair', function(done){
@@ -90,6 +147,12 @@ describe('asset calucaltor', function(){
 		earning.should.be.equal(0);
 		done();
 	})
+  it('verify config.bonus', function(done){//活期
+    var earning = calculateSingleUserAsset(funds[1], testAssemble(bonusSample));
+    console.log("earning", earning);
+    earning.should.be.above(0);
+    done();
+  })
 	
 
 })  
